76187a3b515ad17c62aa753006d33910af129467
Annania Plossl <nia.plossl@gmail.com>
2024-04-24 13:39:27 -0400

    completed the lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,35 @@
 class Pet:
-    pass
+    PET_TYPES = ['dog','cat','rodent','bird','reptile','exotic']
+    all = []
+
+    def __init__(self,name,pet_type,owner=None) -> None:
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+    
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self,pet_type):
+        if pet_type in Pet.PET_TYPES:
+            self._pet_type = pet_type
+        else:
+            raise TypeError
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def get_sorted_pets(self):
+        return sorted(self.pets(),key=lambda x: x.name)
+    
+    def add_pet(self,pet):
+        if not isinstance(pet,Pet):
+            raise TypeError("Must be a Pet")
+        pet.owner = self
